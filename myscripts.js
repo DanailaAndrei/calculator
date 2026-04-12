b5ac8ab3a4ca7f5c00cd894bc4cf2ef71b12cabd
Andrei <danaila95andrei@gmail.com>
2026-04-13 00:27:50 +0300

    almost finished the frontend of the calculator

--- myscripts.js
+++ myscripts.js
@@ -19,3 +19,29 @@ function operator(op, a, b){
 }
 
 let nr1, nr2, op;
+
+let numbersDiv = document.querySelector(".numbers");
+let array = [0, ".", "=", 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+array.forEach(el => {
+    let digitDiv = document.createElement("div");
+    digitDiv.textContent = el;
+    digitDiv.setAttribute("class", "digit");
+    numbersDiv.appendChild(digitDiv);
+});
+
+let operatorsDiv = document.querySelector(".operators");
+array = ["÷", "x", "-", "+"];
+
+array.forEach(el => {
+    let opDiv = document.createElement("div");
+    opDiv.textContent = el;
+    opDiv.setAttribute("class", "operator");
+    operatorsDiv.appendChild(opDiv);
+});
+
+let clearDiv = document.querySelector(".clear");
+let acDiv = document.createElement("div");
+acDiv.textContent = "AC";
+acDiv.setAttribute("class", "ac");
+clearDiv.appendChild(acDiv);
